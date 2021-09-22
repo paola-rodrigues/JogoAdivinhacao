d8b260e851c6c2d302fa6747acbcb79e88639855
paola-rodrigues <paola.n.rodrigues@gmail.com>
2021-09-22 17:59:46 -0300

    Update adivinhacao.py

--- adivinhacao.py
+++ adivinhacao.py
@@ -2,7 +2,7 @@ print("**********************************")
 print('Bem-vindo ao Jogo de Adivinhação!')
 print("**********************************")
 
-print ("estou pensando no número entre 0 e 20")
+print ("Estou pensando no número entre 0 e 20")
 
 numero_secreto     = 9
 total_de_tentativas = 4
@@ -11,10 +11,13 @@ rodada              = 1
 for rodada in range (1, total_de_tentativas + 1):
     print ("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute = input("Digite o seu numero: ")
-   
-
     print("Você digitou ", chute)
     numero = int(chute)
+    
+    if(numero < 1 or numero > 20):
+        print ("Você deve digitar um número entre 0 e 20!")
+
+    
 
     acertou = numero == numero_secreto
     maior   = numero > numero_secreto
@@ -22,6 +25,7 @@ for rodada in range (1, total_de_tentativas + 1):
 
     if(acertou):
         print("Você acertou")
+        break;
     else:
         if(maior):
             print("Você errou! O seu chute foi maior do que o número secreto.")
