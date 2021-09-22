85f5409da5c159efa61cd43fa49c4c500e243eed
paola-rodrigues <paola.n.rodrigues@gmail.com>
2021-09-22 15:26:07 -0300

    Create adivinhacao.py

---
+++ adivinhacao.py
@@ -0,0 +1,24 @@
+print("**********************************")
+print('Bem-vindo ao Jogo de Adivinhação!')
+print("**********************************")
+
+print ("estou pensando no número entre 0 e 20")
+print()
+numero_secreto = 2
+
+chute = input("Digite o seu numero: ")
+
+print("Você digitou ", chute)
+numero = int(chute)
+
+if(numero_secreto == numero):
+    print("Você acertou")
+else:
+    print("Você errou")
+    
+
+
+ 
+
+    
+    
