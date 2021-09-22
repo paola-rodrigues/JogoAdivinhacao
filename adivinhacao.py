e8ce32b4cee2e6f58c957b8b96957c55a756ef4d
paola-rodrigues <paola.n.rodrigues@gmail.com>
2021-09-22 17:28:23 -0300

    Update adivinhacao.py

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,24 +4,30 @@ print("**********************************")
 
 print ("estou pensando no número entre 0 e 20")
 
-numero_secreto = 9
+numero_secreto     = 9
+total_de_tentativas = 4
+rodada              = 1
 
-chute = input("Digite o seu numero: ")
+while(rodada <= total_de_tentativas):
+    print ("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute = input("Digite o seu numero: ")
+   
 
-print("Você digitou ", chute)
-numero = int(chute)
+    print("Você digitou ", chute)
+    numero = int(chute)
 
-acertou = numero == numero_secreto
-maior   = numero > numero_secreto
-menor   = numero < numero_secreto
+    acertou = numero == numero_secreto
+    maior   = numero > numero_secreto
+    menor   = numero < numero_secreto
 
-if(acertou):
-    print("Você acertou")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior do que o número secreto.")
-    elif(menor):
-        print("Você errou! O seu chute foi menor do que o número secreto.")
+    if(acertou):
+        print("Você acertou")
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif(menor):
+            print("Você errou! O seu chute foi menor do que o número secreto.")
+    rodada = rodada + 1    
     
 print("Fim do Jogo")    
     
