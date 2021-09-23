4cc9a4876d8f9eecd45c6eeaea789460daca783e
paola-rodrigues <paola.n.rodrigues@gmail.com>
2021-09-22 22:52:56 -0300

    Update adivinhacao.py

--- adivinhacao.py
+++ adivinhacao.py
@@ -8,10 +8,22 @@ print("**********************************")
 print ("Estou pensando no número entre 0 e 20")
 
 numero_secreto     = random.randrange(1,21)
-total_de_tentativas = 4
+total_de_tentativas = 0
 rodada              = 1
+pontos = 100
 
+print ("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Díficil")
 
+nivel = int(input("Defina o nível: "))
+
+if(nivel == 1):
+    total_de_tentativas = 10
+elif(nivel == 2):
+    total_de_tentativas = 6
+else:
+    total_de_tentativas = 4  
+    
 
 for rodada in range (1, total_de_tentativas + 1):
     print ("Tentativa {} de {}".format(rodada, total_de_tentativas))
@@ -29,14 +41,15 @@ for rodada in range (1, total_de_tentativas + 1):
     menor   = numero < numero_secreto
 
     if(acertou):
-        print("Você acertou")
+        print("Você acertou e fez {} pontos!".format(pontos))
         break;
     else:
         if(maior):
             print("Você errou! O seu chute foi maior do que o número secreto.")
         elif(menor):
             print("Você errou! O seu chute foi menor do que o número secreto.")
-  
+        pontos_perdidos = abs (numero_secreto - numero)   
+        pontos = pontos - pontos_perdidos 
     
 print("Fim do Jogo")    
     
