1a609e43c198fd6f57aebe9f426f7369982fa7f1
paola-rodrigues <paola.n.rodrigues@gmail.com>
2021-09-23 19:50:17 -0300

    Update jogoForca.py

--- jogoForca.py
+++ jogoForca.py
@@ -5,6 +5,27 @@ def jogar():
     print("****Bem-Vindo no jogo de Forca****!")
     print("**********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou  = False
+
+    #enquato não enforcou e não acertou
+    while(not enforcou and not acertou):
+        
+        chute = input ("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format (letra, index))
+            index = index + 1    
+                
+        print("jogando...")
+
+        
+
     print("Fim do jogo")      
       
       
